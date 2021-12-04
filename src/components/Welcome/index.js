700d8d328eaf41e558b5f3b2dd1faccbc17e47f2
sdenvs <sdenvs@gmail.com>
2021-12-04 13:31:07 +0530

    changed

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,1 +1,31 @@
 // Write your code here
+import './index.css'
+import {Component} from 'react'
+
+class Welcome extends Component {
+  state = {subscribed: false}
+
+  clicked = () => {
+    const {subscribed} = this.state
+    if (subscribed) {
+      this.setState(prevObj => ({subscribed: false}))
+    } else {
+      this.setState(prevObj => ({subscribed: true}))
+    }
+  }
+
+  render() {
+    const {subscribed} = this.state
+    return (
+      <div className="bgContainer">
+        <h1 className="heading">Welcome</h1>
+        <p className="paragraph">Thank you! Happy Learning</p>
+        <button onClick={this.clicked} className="button">
+          {subscribed ? 'Subscribed' : 'Subscribe'}
+        </button>
+      </div>
+    )
+  }
+}
+
+export default Welcome
